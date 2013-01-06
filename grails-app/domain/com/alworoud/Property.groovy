1d86d7edc177a6a5349078838229c3833649bd89
suryazi <suryazi@abg.com.sa>
2013-01-06 17:16:16 +0300

    Added remarks field

--- grails-app/domain/com/alworoud/Property.groovy
+++ grails-app/domain/com/alworoud/Property.groovy
@@ -1,17 +1,21 @@
 package com.alworoud
 
 class Property {
-
     String propType
     String street
     String city
     Double area
     String status
+    String remarks
     Date dateCreated
     Date lastUpdated
     static constraints = {
-        propType(blank:false)
+        propType(blank:false, inList:(['Building','Compound','Land']))
+        street(nullable:true)
+        city(nullable:true)
+        area(nullable:true)
         status(blank:false,inList:(['Vacant','Rent','Lease','Maintenance','Sale','Sold']))
+        remarks(nullable:true)
     }
     static belongsTo = [owners:Owner]
     static hasMany = [owners:Owner, units:Unit]
--- grails-app/domain/com/alworoud/Unit.groovy
+++ grails-app/domain/com/alworoud/Unit.groovy
@@ -1,16 +1,19 @@
 package com.alworoud
 
 class Unit {
-
     String unitType
-    String Desc
+    String desc
     Double area
     String status
+    String remarks
     Date dateCreated
     Date lastUpdated
     static constraints = {
-        unitType(blank:false)
+        unitType(blank:false,inList:(['Villa','Apartment','Shop','Space','Land','Building']))
+        desc(nullable: true)
+        area(nullable: true)
         status(blank:false,inList:(['Vacant','Rent','Lease','Maintenance','Sale','Sold']))
+        remarks(nullable:true)
     }
     static belongsTo = [prop : Property]
 }
